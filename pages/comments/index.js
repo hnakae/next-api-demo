8904ff82cd6a4e7646eb2131fb188557b3a148c0
hnakae <nakae27@gmail.com>
2022-07-10 15:10:46 -0700

    dynamic api routes

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function CommentsPage() {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
 
   const fetchComments = async () => {
     const response = await fetch("/api/comments");
@@ -9,8 +10,27 @@ function CommentsPage() {
     setComments(data);
   };
 
+  const submitComment = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ text: comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <button onClick={submitComment}>Submit comment</button>
       <button onClick={fetchComments}>Load comments</button>
       {comments.map((comment) => (
         <div key={comment.id}>
